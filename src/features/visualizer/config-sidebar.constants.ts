ba7f3d5b2f68e576c0c0db7797124b2c3d3574a6
Merry Rosalie <merryrosalie001@gmail.com>
2025-06-29 21:31:59 +1000

    feat: implemented radio component

---
+++ src/features/visualizer/config-sidebar.constants.ts
@@ -0,0 +1,14 @@
+export const GRAVITY = {
+  ZERO_GRAVITY: 0,
+  LOW_GRAVITY: 1,
+  HIGH_GRAVITY: 2,
+};
+
+export const NODE_SCALAR_SIZE = {
+    INVISIBLE: 0,
+    EXTRA_SMALL: 1,
+    SMALL: 2,
+    MEDIUM: 3,
+    LARGE: 4,
+    EXTRA_LARGE: 5
+}
--- src/features/visualizer/config-sidebar.tsx
+++ src/features/visualizer/config-sidebar.tsx
@@ -1,4 +1,6 @@
 import { ChevronsLeft, ChevronsRight, Settings } from "lucide-react";
+import { Label } from "~/components/form/label";
+import { RadioGroup, RadioGroupItem } from "~/components/form/radio-group";
 import { Separator } from "~/components/ui/separator";
 import {
   Sidebar,
@@ -8,21 +10,85 @@ import {
   useSidebar,
 } from "~/components/ui/sidebar";
 import { useIsMobile } from "~/hooks/use-mobile";
+import { GRAVITY, NODE_SCALAR_SIZE } from "./config-sidebar.constants";
+
+const RADIO_GROUPS = [
+  {
+    title: "Gravity Strength",
+    description: "Modifies the gravitation strength of the center of the graph",
+    defaultValue: GRAVITY.ZERO_GRAVITY,
+    values: [
+      {
+        label: "Zero Gravity (Default)",
+        value: GRAVITY.ZERO_GRAVITY,
+      },
+      {
+        label: "Low Gravity",
+        value: GRAVITY.LOW_GRAVITY,
+      },
+      {
+        label: "High Gravity",
+        value: GRAVITY.HIGH_GRAVITY,
+      },
+    ],
+  },
+  {
+    title: "Node Scalar Size",
+    description: "Modify the sizes for all nodes",
+    defaultValue: NODE_SCALAR_SIZE.MEDIUM,
+    values: [
+      {
+        label: "Invisible",
+        value: NODE_SCALAR_SIZE.INVISIBLE,
+      },
+      {
+        label: "Extra Small",
+        value: NODE_SCALAR_SIZE.EXTRA_SMALL,
+      },
+      {
+        label: "Small",
+        value: NODE_SCALAR_SIZE.SMALL,
+      },
+      {
+        label: "Medium (Default)",
+        value: NODE_SCALAR_SIZE.MEDIUM,
+      },
+      {
+        label: "Large",
+        value: NODE_SCALAR_SIZE.LARGE,
+      },
+      {
+        label: "Extra Large",
+        value: NODE_SCALAR_SIZE.EXTRA_LARGE,
+      },
+    ],
+  },
+];
 
 export default function ConfigSidebar() {
   return (
     <SidebarProvider name="config-sidebar" className="relative isolate z-10">
       <Sidebar side="right">
-        <SidebarContent className="p-4">
+        <SidebarContent className="p-6 space-y-4">
           <h1 className="medium-title">Graph Options</h1>
-
-          {/* Gravity Strength */}
-          <h2 className="small-title">Gravity Strength</h2>
-          <p>Modifies the gravitation strength of the center of the graph</p>
-
-          {/* Node Scalar Size */}
-          <h2 className="small-title">Node Scalar Size</h2>
-          <p>Modify the sizes for all nodes</p>
+          {RADIO_GROUPS.map(({ title, description, defaultValue, values }) => (
+            <div key={title} className="space-y-3">
+              <h2 className="small-title">{title}</h2>
+              <p className="small-body text-typography-secondary">
+                {description}
+              </p>
+              <RadioGroup defaultValue={String(defaultValue)}>
+                {values.map(({ label, value }) => (
+                  <div className="flex items-center gap-2">
+                    <RadioGroupItem value={String(value)} id={label} />
+                    <Label className="font-normal" htmlFor={label}>
+                      {label}
+                    </Label>
+                  </div>
+                ))}
+              </RadioGroup>
+            </div>
+          ))}
         </SidebarContent>
       </Sidebar>
       <ConfigSidebarControls />
