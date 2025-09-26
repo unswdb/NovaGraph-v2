a0e9059a2c98070ad1b0d68a4645e28c84fa1b89
Mason Truong <z5381092@ad.unsw.edu.au>
2025-09-26 21:15:16 +1000

    fix: temporary add dirty test to VisualizeStore to test

--- src/features/visualizer/store.ts
+++ src/features/visualizer/store.ts
@@ -60,6 +60,94 @@ export default class VisualizerStore {
 
     // Define initial graph structure
     const graph = await this.controller.initGraph();
+
+    // console.warn('Testing createSchema2...');
+   
+    // // ----------------------------
+    // // 1. Create Person table (many primitives)
+    // // ----------------------------
+    // const personResult = await controller.db.createSchema(
+    //   'node',
+    //   'Person',
+    //   'name', // primaryKey
+    //   {
+    //     name: 'STRING',
+    //     age: 'INT',
+    //     alive: 'BOOLEAN',
+    //     height: 'DOUBLE',
+    //     uuid: 'UUID',
+    //   }
+    // );
+    // console.warn('Person schema result:', personResult);
+    
+    // const addressResult = await controller.db.createSchema(
+    //   "node",
+    //   "Address",
+    //   "id", // primaryKey
+    //   {
+    //     id: "STRING",
+    //     location: {
+    //       kind: "STRUCT",
+    //       fields: {
+    //         street: "STRING",
+    //         city: "STRING",
+    //         zipcode: "INT",
+    //       },
+    //     },
+    //   }
+    // );
+    // console.warn("Address schema result:", addressResult);
+
+    // const personQuery = await controller.db.createNode('Person', {
+    //   name:  ['STRING',  'Tom Hanks'],
+    //   age:   ['INT',     67],
+    //   alive: ['BOOLEAN', true],
+    //   height:['DOUBLE',  1.83],
+    //   uuid:  ['UUID',    '123e4567-e89b-12d3-a456-426614174000']
+    // });
+    // console.warn('Person create query:', personQuery);
+
+    // const addressQuery = await controller.db.createNode('Address', {
+    //   id: ['STRING', 'ADDR-001'],   // primary key
+    //   location: [
+    //     { kind: 'STRUCT', fields: {
+    //       street: 'STRING',
+    //       city:   'STRING',
+    //       zipcode:'INT'
+    //     }},
+    //     {
+    //       street:  '123 Main St',
+    //       city:    'Los Angeles',
+    //       zipcode: 90001
+    //     }
+    //   ]
+    // });
+   
+    // console.warn('Address create query:', addressQuery);
+   
+
+
+    // const addressQuery2 = await controller.db.createNode('Address', {
+    //   id: ['STRING', 'ADDR-0012'],   // primary key
+    //   // id2: ['STRING', 'ADDR-00123'],   // primary key
+
+
+    //   // location: [
+    //   //   { kind: 'STRUCT', fields: {
+    //   //     street: 'STRING',
+    //   //     city:   'STRING',
+    //   //     zipcode:'INT'
+    //   //   }},
+    //   //   {
+    //   //     street:  '123 Main St',
+    //   //     city:    'Los Angeles',
+    //   //     zipcode: 90001
+    //   //   }
+    //   // ]
+    // });
+   
+    // console.warn('Address create query:', addressQuery2);
+
     runInAction(() => {
       // TODO: Change to controller helper function that retrieves all the database list
       this.databases = [
@@ -84,7 +172,7 @@ export default class VisualizerStore {
   };
 
   cleanup = () => {
-    controller.cleanup();
+    // controller.cleanup();
   };
 
   setDatabase = (database: GraphDatabase) => {
