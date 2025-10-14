8cd9a71f2dc94bfb23f6e89bcd8f608fac9e7dcd
Merry Rosalie <merryrosalie001@gmail.com>
2025-10-14 12:51:16 +0700

    chore: make primary key non-editable and pass in source and target into create edge

--- src/features/visualizer/renderer/node-metadata/attributes.tsx
+++ src/features/visualizer/renderer/node-metadata/attributes.tsx
@@ -31,6 +31,7 @@ export default function AttributesForm({
       key: nodeSchema.primaryKey,
       displayName: capitalize(nodeSchema.primaryKey),
       defaultValue: node._primaryKeyValue,
+      disabled: true,
     }),
     ...Object.entries(nodeSchema.properties).map(([key, type]) =>
       createSchemaInput(type, {
